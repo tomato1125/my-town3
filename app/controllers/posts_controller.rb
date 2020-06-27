32ca72af3f842b4cd6104b91bfde86fd342e32c1
tomato1125 <naokiyoshida1125@icoud.com>
2020-06-27 17:35:30 +0900

    front

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show, :search]
+
+  def index
+    @posts = Post.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comments = @post.comments.includes(:user)
+  end
+
+  def search
+    @posts = Post.search(params[:keyword])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :image, :content, :prefecture_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+end
