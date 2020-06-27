5eda3edca58dbb84175b413b83d6509f269b9428
tomato1125 <naokiyoshida1125@icoud.com>
2020-06-27 17:48:17 +0900

    front2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
-  validates :text, presence: true
+  belongs_to :user, optional: true
+  has_many :comments, foreign_key: :post_id
+
+  def self.search(search)
+    return Post.all unless search
+    Post.where('text LIKE(?)', "%#{search}%")
+  end
+  
 end
