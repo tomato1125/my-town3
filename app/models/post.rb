fd3f1bda5551e81ccb9b708297652fc2c78c95fe
tomato1125 <naokiyoshida1125@icoud.com>
2020-07-02 21:57:25 +0900

    change

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
 
   def self.search(search)
     return Post.all unless search
-    Post.where('text LIKE(?)', "%#{search}%")
+    Post.where('content LIKE(?)', "%#{search}%")
   end
   
 end
