96ae45c6dd1b285ad72f04d379ad1c0ef7332e3d
tomato1125 <naokiyoshida1125@icoud.com>
2020-07-29 20:46:07 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
   validates :title,presence: true
   validates :content,presence: true
   validates :image,presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
-  validates :maptitle, presence: true
+  # validates :latitude, presence: true
+  # validates :longitude, presence: true
+  # validates :maptitle, presence: true
 
   belongs_to :user
   belongs_to :prefecture
