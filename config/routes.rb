68bbf2f4ef4c80c21f123edb138777e10083febe
tomato1125 <naokiyoshida1125@icoud.com>
2020-08-04 19:36:01 +0900

    mypage_function

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-  resources :users, only: :show
+  resources :users, only: [:index, :show]
 
   resources :categories, only: [:index, :show]
 
