c25d288a518b9ae1046bf072380bc81a54389d94
tomato1125 <naokiyoshida1125@icoud.com>
2020-08-06 12:04:41 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'mytown3'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mytown'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mytown3'
 end
