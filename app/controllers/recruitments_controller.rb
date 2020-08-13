5ed18fcb349dc50987c5dc11f471be00d537c090
tomato1125 <naokiyoshida1125@icoud.com>
2020-08-13 13:23:27 +0900

    fix

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -8,6 +8,10 @@ class RecruitmentsController < ApplicationController
     Recruitment.create(recruitment_params)
   end
 
+  def show
+    @recruitment = Recruitment.find(params[:id])
+  end
+
   private
   def recruitment_params
     params.require(:recruitment).permit(:campany, :image, :numberOfEmployees, :feature, :workLocation, :eligibleEmployees, :workingHours, :insurance, :wellfare, :prefecture_id, :industry_id, :position_id, :occupation_id, :income_id, :title, :jobDescription, :vacation).merge(user_id: current_user.id)
